0cbe6000dabc0f7d92d1b15fb262b36cc3683c56
Eng. Lenny Ng'ang'a <33583893+codewithlennylen@users.noreply.github.com>
2020-06-27 03:51:01 +0300

    Created Data Method to be used in parsing the data fetched from the API
    The Data class has multiple methods that 'filter' through various data items in the API response such as 'total_cases', 'total_deaths', 'get_country_data', etc

--- main.py
+++ main.py
@@ -8,3 +8,48 @@ RUN_TOKEN = ''
 
 response = requests.get(f'https://www.parsehub.com/api/v2/projects/{PROJECT_TOKEN}/last_ready_run/data', params={'api_key' : API_KEY})
 data = json.loads(response.text)
+
+
+class Data:
+	def __init__(self, api_key, project_token):
+		self.api_key = api_key
+		self.project_token = project_token
+		self.params = {
+			"api_key" : self.api_key
+		}
+		self.get_data()
+
+	def get_data(self):
+		response = requests.get(f'https://www.parsehub.com/api/v2/projects/{self.project_token}/last_ready_run/data', params=self.api_key)
+		self.data = json.loads(response.text)
+
+	def get_total_cases(self):
+		data = self.data['total']
+
+		for content in data:
+			if content['name'] == 'Coronavirus Cases:':
+				return content['value']
+		return "0"
+
+	def get_total_deaths(self):
+		data = self.data['total']
+
+		for content in data:
+			if content['name'] == 'Deaths:':
+				return content['value']
+		return "0"
+	
+	def get_country_data(self, country):
+		data = self.data['country']
+
+		for content in data:
+			if content['name'].lower() == country.lower():
+				return content
+		return "0"
+
+
+data = Data(API_KEY, PROJECT_TOKEN)
+# print(data.data)
+print(data.get_total_cases())
+print(data.get_total_deaths())
+print(data.get_country_data('canada')['total_cases'])
